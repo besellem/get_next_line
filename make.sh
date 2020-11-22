7c53ef72891612781ad751e9638e36ea1246332a
besellem <[besellem@student.42.fr]>
2020-11-22 19:26:07 +0100

    cleaned

--- make.sh
+++
@@ -1,3 +0,0 @@
-# -Wall -Wextra -Werror -fsanitize=address
-gcc -D BUFFER_SIZE=$1 get_next_line.c get_next_line_utils.c -I get_next_line.h
-# gcc -Wall -Wextra -Werror -fsanitize=address get_next_line_bonus.c get_next_line_utils_bonus.c -I get_next_line_bonus.h
